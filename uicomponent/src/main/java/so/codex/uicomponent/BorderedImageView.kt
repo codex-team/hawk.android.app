6c9a58920a26ddafd0a620cf64c3c9b8d1248a83
Shiplayer <shiplayer@yandex.ru>
2019-08-06 18:21:10 +0300

    update bordered image view

--- uicomponent/src/main/java/so/codex/uicomponent/BorderedImageView.kt
+++ uicomponent/src/main/java/so/codex/uicomponent/BorderedImageView.kt
@@ -3,30 +3,38 @@ package so.codex.uicomponent
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
-import android.view.View
 import android.widget.ImageView
 
 class BorderedImageView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr){
+) : ImageView(context, attrs, defStyleAttr) {
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
     }
 
-    private val mRect: Rect = Rect(30,30,width - 30,height - 30)
+    private val mRect: RectF by lazy {
+        RectF(0f, 0f, width.toFloat(), height.toFloat())
+    }
+
+    private val corners by lazy {
+        5f * resources.displayMetrics.density
+    }
     private val mPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = Color.GREEN
     }
+
+    private val mPath = Path()
 
     private val mPaintCircle = Paint().apply {
         color = Color.RED
     }
 
     override fun onDraw(canvas: Canvas?) {
+        mPath.rewind()
+        mPath.addRoundRect(mRect, corners, corners, Path.Direction.CCW)
+        canvas?.clipPath(mPath)
         super.onDraw(canvas)
 
-        canvas?.drawCircle(width / 2f, height / 2f, 25f, mPaintCircle)
-        canvas?.drawRect(mRect, mPaint)
     }
 }
