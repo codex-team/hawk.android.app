ed8df46bfa698852b541e32dc8322c3a5375f79d
Shiplayer <shiplayer@yandex.ru>
2019-10-04 18:19:10 +0300

    add data class for project with common information

--- codexbl/src/main/java/so/codex/codexbl/entity/Project.kt
+++ codexbl/src/main/java/so/codex/codexbl/entity/Project.kt
@@ -1,11 +1,31 @@
 package so.codex.codexbl.entity
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 data class Project(
-        val id: String,
-        val token: String,
-        val name: String,
-        val description: String,
-        val url: String,
-        val image: String,
-        val events: List<Event>
+    override val id: String,
+    override val token: String,
+    override val name: String,
+    override val description: String,
+    override val url: String,
+    override val image: String,
+    val events: List<Event>
+) : ProjectCommon(
+    id,
+    token,
+    name,
+    description,
+    url,
+    image
 )
+
+@Parcelize
+open class ProjectCommon(
+    open val id: String,
+    open val token: String,
+    open val name: String,
+    open val description: String,
+    open val url: String,
+    open val image: String
+) : Parcelable
--- codexbl/src/main/java/so/codex/codexbl/view/workspace/IWorkspaceView.kt
+++ codexbl/src/main/java/so/codex/codexbl/view/workspace/IWorkspaceView.kt
@@ -1,5 +1,6 @@
 package so.codex.codexbl.view.workspace
 
+import so.codex.codexbl.entity.Project
 import so.codex.codexbl.entity.Workspace
 import so.codex.codexbl.view.base.ILoaderView
 
@@ -7,4 +8,6 @@ interface IWorkspaceView : ILoaderView {
     fun showWorkspaces(workspace: List<Workspace>)
 
     fun showEmptyWorkspace()
+
+    fun openProject(project: Project)
 }
