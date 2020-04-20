0c6c65ce7e51262249c7029fd6a07331c6542d0d
Shiplayer <shiplayer@yandex.ru>
2020-04-20 12:48:32 +0300

    add outline for image with corners, upgrade min api

--- uicomponent/src/main/java/so/codex/uicomponent/BorderedImageView.kt
+++ uicomponent/src/main/java/so/codex/uicomponent/BorderedImageView.kt
@@ -1,16 +1,11 @@
 package so.codex.uicomponent
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffXfermode
-import android.graphics.RectF
+import android.graphics.Outline
 import android.util.AttributeSet
 import android.view.View
-import android.widget.ImageView
+import android.view.ViewOutlineProvider
+import androidx.appcompat.widget.AppCompatImageView
 
 /**
  * Simple image view with rounded corners
@@ -18,16 +13,20 @@ import android.widget.ImageView
  */
 class BorderedImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = R.attr.borderedImageViewStyle
-) : ImageView(context, attrs, defStyleAttr) {
+) : AppCompatImageView(context, attrs, defStyleAttr) {
     /**
      * Radius of rounded corners
      */
     private var mCorners = 0f
-    private val xfermode = PorterDuffXfermode(PorterDuff.Mode.XOR)
+
+    /**
+     * Screen density of phone
+     */
+    private val density by lazy {
+        resources.displayMetrics.density
+    }
 
     init {
-        setLayerType(View.LAYER_TYPE_SOFTWARE, null)
-
         context.obtainStyledAttributes(
             attrs,
             R.styleable.BorderedImageView,
@@ -36,44 +35,21 @@ class BorderedImageView @JvmOverloads constructor(
         ).apply {
 
             mCorners = getDimension(R.styleable.BorderedImageView_corners, 0f)
-
             recycle()
         }
-    }
 
-    /**
-     * Brush that painted of mask for image view
-     */
-    private val mPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        xfermode = this@BorderedImageView.xfermode
-    }
-
-    /**
-     * lazy initializing of mask
-     */
-    private val mask: Bitmap by lazy {
-        val mask = Bitmap.createBitmap(
-            width - paddingRight - paddingLeft,
-            height - paddingBottom - paddingTop,
-            Bitmap.Config.ARGB_8888
-        )
-        Canvas(mask).apply {
-            val rect = RectF(0f, 0f, mask.width.toFloat(), mask.height.toFloat())
-            val paint = Paint()
-            paint.isAntiAlias = true
-            paint.xfermode = null
-            paint.color = Color.BLACK
-            drawRoundRect(rect, mCorners, mCorners, paint)
-            paint.xfermode = xfermode
-            paint.color = Color.WHITE
-            drawRect(rect, paint)
-            paint.xfermode = null
+        outlineProvider = object : ViewOutlineProvider() {
+            override fun getOutline(view: View, outline: Outline) {
+                outline.setRoundRect(
+                    view.left + view.paddingLeft,
+                    view.top + view.paddingTop,
+                    view.right - view.paddingRight,
+                    view.bottom - view.paddingBottom,
+                    mCorners
+                )
+            }
         }
-        mask
-    }
-
-    private val density by lazy {
-        resources.displayMetrics.density
+        clipToOutline = true
     }
 
     /**
@@ -83,13 +59,5 @@ class BorderedImageView @JvmOverloads constructor(
         mCorners = dp * density
 
         invalidate()
-    }
-
-    /**
-     * Draw image on canvas
-     */
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-        canvas?.drawBitmap(mask, 0f, 0f, mPaint)
     }
 }
